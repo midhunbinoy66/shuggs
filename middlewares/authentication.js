e6175fe8bb402a24fca0c298fcdd4b2c9da7457a
midhun Binoy <midhunbinoy508@gmail.com>
2023-12-06 09:17:24 +0530

    removed auth confirmation

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -12,10 +12,6 @@ if(!token){
 try {
     const payload =jwt.verify(token,process.env.JWT_SECRET);
     req.user ={userId:payload.userId,name:payload.name,role:payload.role,email:payload.email};
-    const user = await User.findById({_id:payload.userId});
-    if(user.blocked===true){
-        throw new Error;
-    }
     next();
 
 } catch (error) {
