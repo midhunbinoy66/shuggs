582f289533513c82085df7341b33327650ceb614
midhun Binoy <midhunbinoy508@gmail.com>
2023-12-06 07:26:52 +0530

    block criteria in middleware,some other fixes

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const badRequetsError = require('../errors/badrequest');
 const cutsomeAPIError = require('../errors/customeAPIError');
+const User = require('../models/user');
 
 const adminAuth = async (req,res,next)=>{
 const token = req.cookies.token;
@@ -11,6 +12,10 @@ if(!token){
 try {
     const payload =jwt.verify(token,process.env.JWT_SECRET);
     req.user ={userId:payload.userId,name:payload.name,role:payload.role,email:payload.email};
+    const user = await User.findById({_id:payload.userId});
+    if(user.blocked===true){
+        throw new Error;
+    }
     next();
 
 } catch (error) {
